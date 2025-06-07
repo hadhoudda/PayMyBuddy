ce7ef9c918221e4db37a139ecb174c63fcf65c49
Houda Ben Abdallah <ahhouda@yahoo.fr>
2025-06-08 00:56:30 +0200

    config db

--- src/main/java/com/paymybuddy/PaymybuddyApplication.java
+++ src/main/java/com/paymybuddy/PaymybuddyApplication.java
@@ -3,11 +3,12 @@ package com.paymybuddy;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.Optional;
+
 @SpringBootApplication
-public class PaymybuddyApplication {
+public class PaymybuddyApplication{
 
 	public static void main(String[] args) {
 		SpringApplication.run(PaymybuddyApplication.class, args);
 	}
-
 }
